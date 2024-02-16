43e44a0f909d0f5e7309dcde3c1e3c6fe7311875
xiangshangzy <13410704085@163.com>
2024-02-17 00:11:55 +0800

    update

--- src/app/(tab)/index.tsx
+++ src/app/(tab)/index.tsx
@@ -1,25 +1,16 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { router } from 'expo-router';
-import { Image, Pressable, View, Text } from 'react-native';
-
-function LogoTitle() {
-  return (
-    <Image
-      style={{ width: 50, height: 50 }}
-      source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-    />
-  );
-}
+import { Image, Pressable, View, Text, ScrollView } from 'react-native';
 
 export default function Home() {
 
   return (
-    <View className='flex-1 items-center text-white'>
+    <View className='flex-1 items-cente' >
       <Header />
       <View className='w-full px-6'>
         <Text className='text-xl'>Newest Songs</Text>
-        <View>
-          <MusicItem />
+        <View className='flex-row overflow-x-auto overflow-hidden gap-x-4'>
+          {[1, 2, 3, 4, 5].map((item, index) => <MusicItem key={index} />)}
         </View>
       </View>
     </View>
@@ -28,7 +19,7 @@ export default function Home() {
 
 function Header() {
   return (
-    <View className='h-fit bg-gradient-to-b from-emerald-800 p-6 w-full'>
+    <View className='h-fit bg-gradient-to-b from-emerald-800 p-6 w-full '>
       <View>
         <Text className=' text-white text-3xl font-semibold mb-2'>Welcome back</Text>
         <Pressable className='relative h-14 group flex flex-row items-center rounded-md  gap-x-6 bg-neutral-100/10 active:bg-neutral-100/20 transition w-fit pr-4'>
@@ -44,8 +35,10 @@ function Header() {
 }
 function MusicItem() {
   return (
-    <Pressable className=' bg-neutral-400/5 active:bg-neutral-400/10 transition p-3'>
-      <Image />
-    </Pressable>
+    <View className='rounded-md group bg-neutral-100 active:bg-neutral-300 transition p-2 w-fit shadow-red-400'>
+      <Image source={require('@/assets/image/music_cover.jpg')} className='object-cover max-w-[120px] max-h-[120px]' />
+      <Text className='font-semibold  mt-2 w-fit'>All of Me</Text>
+      <Text className='text-sm my-1'>author</Text>
+    </View>
   )
 }
